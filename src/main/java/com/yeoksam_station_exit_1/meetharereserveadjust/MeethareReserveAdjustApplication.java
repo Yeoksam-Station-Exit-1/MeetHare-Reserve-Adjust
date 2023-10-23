5dad993f83965b817762789e9c4ecbd6c016f4dc
TProgramer <tposter@naver.com>
2023-10-24 03:04:16 +0900

    Set-up new springboot web project with JPA & MySQL

---
+++ src/main/java/com/yeoksam_station_exit_1/meetharereserveadjust/MeethareReserveAdjustApplication.java
@@ -0,0 +1,13 @@
+package com.yeoksam_station_exit_1.meetharereserveadjust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeethareReserveAdjustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeethareReserveAdjustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeoksam_station_exit_1/meetharereserveadjust/MeethareReserveAdjustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoksam_station_exit_1.meetharereserveadjust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeethareReserveAdjustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
